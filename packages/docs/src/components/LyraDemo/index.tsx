8bc6454b15ca70ac2ac4df03c0bbae60bab5065e
Paolo Insogna <paolo@cowtech.it>
2022-08-03 14:43:48 +0200

    feat: Improve demo page UI.

--- packages/docs/src/components/LyraDemo/index.tsx
+++ packages/docs/src/components/LyraDemo/index.tsx
@@ -1,8 +1,24 @@
 import React, { useEffect, useState } from "react";
 // Had to manually import the ESM vestion to satisfy GitHub actions requirements
-import { create, insert, search, formatNanoseconds } from "@nearform/lyra/dist/esm/lyra";
-import styles from "./style.module.css";
+import { create, formatNanoseconds, insert, search } from "@nearform/lyra/dist/esm/lyra";
 import dataset from "./events";
+import styles from "./style.module.css";
+
+interface Hit {
+  description: string;
+  date: string;
+  granularity: string;
+  categories: {
+    category1: string;
+    category2: string;
+  };
+}
+
+interface SearchResult {
+  count: number;
+  hits: Hit[];
+  elapsed: bigint;
+}
 
 const db = create({
   schema: {
@@ -13,7 +29,7 @@ const db = create({
       category2: "string",
     },
     granularity: "string",
-  }
+  },
 });
 
 function formatYear(date: string) {
@@ -28,55 +44,95 @@ function formatNumber(number: number) {
   return number.toLocaleString();
 }
 
+function Header({ count }: { count: number }) {
+  return (
+    <div className={styles.hero}>
+      <h1> Try Lyra </h1>
+      <p> Type a search term to perform a full-text search on a dataset of {formatNumber(count)} hystorical events </p>
+    </div>
+  );
+}
+
 export function LyraDemo() {
-  const [indexing, setIndexing] = useState(true);
+  const [indexing, setIndexing] = useState(dataset.result.events.length);
   const [term, setTerm] = useState("");
-  const [results, setResults] = useState([]);
-  const [meta, setMeta] = useState({});
+  const [exact, setExact] = useState(false);
   const [limit, setLimit] = useState(10);
   const [offset, setOffset] = useState(0);
   const [tolerance, setTolerance] = useState(0);
-  const [exact, setExact] = useState(false);
+  const [results, setResults] = useState<SearchResult>(null);
 
   useEffect(() => {
-    for (const data of dataset.result.events) {
-      insert(db, {
-        date: data.date,
-        description: data.description,
-        categories: {
-          category1: data.category1,
-          category2: data.category2,
-        },
-        granularity: data.granularity,
-      });
+    function addDocuments() {
+      // We use Random here just to show a nice UI to the user
+      const batch = dataset.result.events.splice(0, 300 + Math.floor(Math.random() * 1000));
+
+      if (!batch.length) {
+        setIndexing(0);
+        return;
+      }
+
+      for (const data of batch) {
+        insert(db, {
+          date: data.date,
+          description: data.description,
+          categories: {
+            category1: data.category1,
+            category2: data.category2,
+          },
+          granularity: data.granularity,
+        });
+      }
+
+      setIndexing(indexing => indexing - batch.length);
+      requestAnimationFrame(addDocuments);
     }
 
-    setIndexing(false);
+    addDocuments();
   }, []);
 
   useEffect(() => {
-    const { hits, count, elapsed } = search(db, {
-      term,
-      limit,
-      offset,
-      exact,
-      tolerance,
-    });
+    if (!term) {
+      setResults(null);
 
-    setMeta({ count, elapsed });
-    setResults(hits);
+      return;
+    }
 
+    setResults(
+      search(db, {
+        term,
+        limit,
+        offset,
+        exact,
+        tolerance,
+      }),
+    );
   }, [term, limit, offset, exact, tolerance]);
+
+  if (indexing > 0) {
+    return (
+      <>
+        <Header count={dataset.result.count} />
+        <div className={styles.container}>
+          <div className={styles.loading}>
+            <h2>
+              Indexing <strong>{formatNumber(indexing)}</strong> events
+            </h2>
+            <p>We will get back to you shortly ...</p>
+          </div>
+        </div>
+      </>
+    );
+  }
 
   return (
     <>
-      <div className={styles.hero}>
-        <h1> Try Lyra </h1>
-        <p> Type a search term to perform a full-text search on a dataset of {formatNumber(dataset.result.count)} hystorical events </p>
-      </div>
+      <Header count={dataset.result.count} />
 
       <div className={styles.container}>
+        <label htmlFor="term">Term</label>
         <input
+          id="term"
           type="text"
           value={term}
           onChange={e => setTerm(e.target.value)}
@@ -87,57 +143,60 @@ export function LyraDemo() {
         <div className={styles.configurations}>
           <div>
             <label htmlFor="exact">Exact</label>
-            <input id="exact" type="checkbox" checked={exact} onChange={() => setExact(!exact)} />
+            <select id="exact" value={exact.toString()} onChange={() => setExact(exact => !exact)}>
+              <option value={"false"}>No</option>
+              <option value={"true"}>Yes</option>
+            </select>
           </div>
           <div>
             <label htmlFor="limit">Limit</label>
-            <input id="limit" type="number" value={limit} onChange={(e) => setLimit(parseInt(e.target.value))} />
+            <input id="limit" type="number" value={limit} onChange={e => setLimit(parseInt(e.target.value))} />
           </div>
           <div>
             <label htmlFor="offset">Offset</label>
-            <input id="offset" type="number" value={offset} onChange={(e) => setOffset(parseInt(e.target.value))} />
+            <input id="offset" type="number" value={offset} onChange={e => setOffset(parseInt(e.target.value))} />
           </div>
           <div>
             <label htmlFor="tolerance">Typo tolerance</label>
-            <input id="tolerance" type="number" value={tolerance} max={3} min={0} onChange={(e) => setTolerance(parseInt(e.target.value))} />
+            <input
+              id="tolerance"
+              type="number"
+              value={tolerance}
+              max={3}
+              min={0}
+              onChange={e => setTolerance(parseInt(e.target.value))}
+            />
           </div>
         </div>
 
-        {
-          indexing && (
-            <div className={styles.loading}>
-              <p>Indexing {formatNumber(dataset.result.count)} events...</p>
+        {results && (
+          <>
+            <h2>
+              Found <strong>{results.count} results</strong> in <strong>{formatNanoseconds(results.elapsed)}</strong>
+            </h2>
+
+            <div className={styles.overflow}>
+              {results.hits.map((result, i) => (
+                <p key={i + result.description} className={styles.result}>
+                  <span className={styles.id}>
+                    Year: <strong>{formatYear(result.date)}</strong>
+                  </span>
+                  <span className={styles.id}>
+                    Category 1: <strong>{result.categories.category1}</strong>
+                  </span>
+                  <span className={styles.id}>
+                    Category 2: <strong>{result.categories.category2}</strong>
+                  </span>
+                  <span className={styles.id}>
+                    Granularity: <strong>{result.granularity}</strong>
+                  </span>
+                  <span className={styles.txt} dangerouslySetInnerHTML={{ __html: result.description }}></span>
+                </p>
+              ))}
             </div>
-          )
-        }
-
-        {
-          !indexing && (
-            <>
-              <div className={styles.meta}>
-                <div>
-                  Results: {(meta as any).count}
-                </div>
-                <div>
-                  Elapsed: {formatNanoseconds((meta as any).elapsed ?? 0)}
-                </div>
-              </div>
-
-              <div className={styles.overflow}>
-                {
-                  results.map((result, i) => (
-                    <p key={i + result.description} className={styles.resultBox}>
-                      <span className={styles.id}> Year: {formatYear(result.date)} </span>
-                      <span className={styles.id}>{result.categories.category1} ({result.categories.category2}). Granularity: {result.granularity}</span>
-                      <span className={styles.txt}>{result.description}</span>
-                    </p>
-                  ))
-                }
-              </div>
-            </>
-          )
-        }
+          </>
+        )}
       </div>
     </>
-  )
+  );
 }
